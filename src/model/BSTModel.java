c39ac0a78ac2739e37e2809ee20596fca8e2713e
Zephaniah Ansah <znansah@wm.edu>
2025-09-30 20:28:25 -0400

    v1.0.1: Add initial structure for BSTModel and TreeNode.

--- src/model/BSTModel.java
+++ src/model/BSTModel.java
@@ -1,4 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+
 public class BSTModel {
+    private TreeNode root;
+
+    public BSTModel() {
+        root = null;
+    }
+
+    // Insert a value
+    public void insert(int value) {
+        // TODO: implement
+    }
+
+    // Traversals
+    public void preorder() { /* TODO */ }
+    public void inorder() { /* TODO */ }
+    public void postorder() { /* TODO */ }
+    public void levelOrder() { /* TODO */ }
+
+    // Balance tree
+    public void balance() {
+        // TODO: implement balancing logic
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
 }
--- src/model/TreeNode.java
+++ src/model/TreeNode.java
@@ -1,4 +1,12 @@
 package model;
 
 public class TreeNode {
+    public int value;
+    public TreeNode left, right;
+    public int x, y;
+    public boolean visited;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
 }
