907dcf43b66a15669c886fa75d0a76293545b81f
Zephaniah Ansah <znansah@wm.edu>
2025-09-30 20:42:23 -0400

    v1.0.2: Add initial structure for BSTController and TreePanel

--- src/controller/BSTController.java
+++ src/controller/BSTController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.BSTModel;
+import model.TreeNode;
+import view.TreePanel;
+
+import javax.swing.*;
+
 public class BSTController {
+    private BSTModel model;
+    private TreePanel view;
+
+    public BSTController(BSTModel model, TreePanel view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void handleInsert(int value) {
+        // TODO: insert into model, update view
+    }
+
+    public void handlePreorder() { /* TODO */ }
+    public void handleInorder() { /* TODO */ }
+    public void handlePostorder() { /* TODO */ }
+    public void handleLevelOrder() { /* TODO */ }
+
+    public void setupUI() {
+        JFrame frame = new JFrame("Dynamic BST Auto-Balance");
+        // TODO: add components, buttons, and listeners
+    }
 }
--- src/view/TreePanel.java
+++ src/view/TreePanel.java
@@ -1,4 +1,29 @@
 package view;
 
-public class TreePanel {
+import model.TreeNode;
+import javax.swing.*;
+import java.awt.*;
+
+public class TreePanel extends JPanel {
+    private TreeNode root;
+
+    public TreePanel(TreeNode root) {
+        this.root = root;
+        setPreferredSize(new Dimension(800, 500));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // TODO: draw tree here
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+        repaint();
+    }
+
+    public void resetVisited(TreeNode node) {
+        // TODO: reset visited flags
+    }
 }
